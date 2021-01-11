b105ab777bf3fe31c6b78dabfb2db3a5c7175719
Katsuhiro <sc30.katsu@gmail.com>
2021-01-11 13:14:12 +0900

    .feat first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 
+// グローバルスタイルはこのファイルに読み込まれる
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
---
+++ pages/blog/[id].js
@@ -0,0 +1,52 @@
+import styles from '../../styles/Home.module.scss'
+
+const BlogId = ({ blog }) => {
+  // dangerouslySetInnerHTMLはローダッシュ2つ
+  return (
+    <main className={ styles.main }>
+      <h1 className={ styles.title }>{blog.title}</h1>
+      <p className={ styles.publishedAt }>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+        className={ styles.post }
+      />
+    </main>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY },
+  }
+
+  const data = await fetch('https://katsu.microcms.io/api/v1/blog', key)
+    .then(res => res.json())
+    .catch(() => null);
+
+  const paths = data.contents.map(content => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async context => {
+  const id = context.params.id;
+  const key = {
+    headers: { 'X-API-KEY' : process.env.API_KEY },
+  }
+
+  const data = await fetch(
+    `https://katsu.microcms.io/api/v1/blog/${ id }`,
+    key,
+  )
+    .then(res => res.json())
+    .catch(() => null);
+
+  return {
+    props: {
+      blog: data,
+    }
+  };
+};
+
+export default BlogId;
