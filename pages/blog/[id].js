48e72c8ca240268e86aa0bfc12f2aa7ed82146aa
Katsuhiro <sc30.katsu@gmail.com>
2021-01-11 13:34:55 +0900

    .feat add categories for each posts

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -5,7 +5,8 @@ const BlogId = ({ blog }) => {
   return (
     <main className={ styles.main }>
       <h1 className={ styles.title }>{blog.title}</h1>
-      <p className={ styles.publishedAt }>{blog.publishedAt}</p>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className={styles.category}>{ blog.category && `${ blog.category.name }` }</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
